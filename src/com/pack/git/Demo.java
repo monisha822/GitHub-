7fa5bc1060945d5874b02b17c2287d794eaa9b58
monis <monis@Monisha>
2025-11-08 08:07:27 +0530

    My Second Commit

--- src/com/pack/git/Demo.java
+++ src/com/pack/git/Demo.java
@@ -2,6 +2,6 @@ package com.pack.git;
 
 public class Demo {
 	public static void main(String[] args) {
-		System.out.println("Hello Java,Good Morning, Had Ur cfe");
+		System.out.println("Hello Java,Good Morning");
 	}
 }
